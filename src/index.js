6757f7b4973b48bcb2905f0ed9e625a0a71b5350
Tomas Zagorskis <tomzagos@gmail.com>
2023-06-23 16:12:15 +0300

    feat: add exit program msg

--- src/index.js
+++ src/index.js
@@ -8,15 +8,29 @@ let userPath = path.dirname(__filename);
 
 const rl = readLine.createInterface({ input, output });
 
+let username;
+
 if (argv[2] && argv[2].startsWith('--username=')) {
-	const username = argv[2].split('=')[1];
-	console.log(`Welcome to the File Manager, ${username}!`);
+	username = argv[2].split('=')[1];
 } else {
-	console.log('Welcome to the File Manager, anonymous!');
+	username = 'Anonymous';
 }
 
+console.log(`\x1b[32mWelcome to the File Manager, ${username}!\n\x1b[0m`);
+console.log(`\x1b[33mYou are currently in ${userPath}\x1b[0m`);
+
 rl.on('line', data => {
-	console.log(`Received: ${data}`);
+	console.log(`Received: ${data}\n`);
 	if (data == 'up') userPath = path.resolve(userPath, '..');
-	console.log(`You are currently in ${userPath}`);
+	if (data == '.exit') {
+		rl.close();
+	} else {
+		console.log(`\x1b[33mYou are currently in ${userPath}\x1b[0m`);
+	}
 });
+
+rl.on('close', () =>
+	console.log(
+		`\x1b[32mThank you for using File Manager, ${username}, goodbye!\x1b[0m`,
+	),
+);
